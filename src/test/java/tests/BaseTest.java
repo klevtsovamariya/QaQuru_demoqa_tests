fc1eee8b5531eea402289e6a39efda2247414dab
Mariia Chumicheva <mariiachu@hflabs.ru>
2026-03-24 15:45:34 +0300

    add Configuration.remote

--- src/test/java/tests/BaseTest.java
+++ src/test/java/tests/BaseTest.java
@@ -15,6 +15,7 @@ public class BaseTest {
         Configuration.browserSize = "1920x1080";
         Configuration.baseUrl = "https://demoqa.com";
         Configuration.pageLoadStrategy = "eager";
+        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
     }
 
     @BeforeEach
