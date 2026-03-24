eb39e135081a66d30d12746539779d7672932810
Mariia Chumicheva <mariiachu@hflabs.ru>
2026-03-24 17:32:43 +0300

    add Attach

--- src/test/java/tests/BaseTest.java
+++ src/test/java/tests/BaseTest.java
@@ -7,7 +7,11 @@ import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
+import org.openqa.selenium.remote.DesiredCapabilities;
 import pages.MainPage;
+import pages.components.Attach;
+
+import java.util.Map;
 
 public class BaseTest {
     MainPage mainPage = new MainPage();
@@ -17,6 +21,14 @@ public class BaseTest {
         Configuration.browserSize = "1920x1080";
         Configuration.baseUrl = "https://demoqa.com";
         Configuration.pageLoadStrategy = "eager";
+
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability("selenoid:options", Map.<String, Object>of(
+                "enableVNC", true,
+                "enableVideo", true
+        ));
+        Configuration.browserCapabilities = capabilities;
+
         Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
     }
 
@@ -27,7 +39,11 @@ public class BaseTest {
     }
 
     @AfterEach
-    void closeWebDriver() {
+    void addAttachments() {
+        Attach.screenshotAs("Last screenshot");
+        Attach.pageSource();
+        Attach.browserConsoleLogs();
+        Attach.addVideo();
         Selenide.closeWebDriver();
     }
-}
+ }
